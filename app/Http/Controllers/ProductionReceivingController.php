3d67b39b02903d2f0747f648bb5bb1222b07e34d
Yousuf Machiyara <m.yousufmachiyara@gmail.com>
2026-06-15 17:22:43 +0500

    Update ProductionReceivingController.php

--- app/Http/Controllers/ProductionReceivingController.php
+++ app/Http/Controllers/ProductionReceivingController.php
@@ -11,6 +11,7 @@ use App\Traits\PostsAccountingEntries;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Log;
+use Carbon\Carbon;
 
 class ProductionReceivingController extends Controller
 {
